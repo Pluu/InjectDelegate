29ba26c3e8b2e370b9f15d3df374a048f2d661a4
pluu <pluulove@gmail.com>
2025-09-27 17:05:01 +0900

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,10 +58,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.bundles.androidX.compose)
 
     implementation(libs.timber)
     implementation(libs.androidx.appcompat)
@@ -73,9 +70,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.bundles.androidX.compose.ui.test)
+    debugImplementation(libs.bundles.androidX.compose.debug)
 
     debugImplementation(libs.androidx.fragment.testing.manifest)
     androidTestImplementation(libs.androidx.fragment.testing)
