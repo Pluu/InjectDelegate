a2d24460c85cf0c12b265d4ae3c07031e24f2919
pluu <pluulove@gmail.com>
2026-04-12 18:34:34 +0900

    Add logging

--- app/src/debug/java/com/pluu/sample/saved/SampleActivity.kt
+++ app/src/debug/java/com/pluu/sample/saved/SampleActivity.kt
@@ -1,9 +1,11 @@
 package com.pluu.sample.saved
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.pluu.sample.saved.model.UserDto
 import com.pluu.sample.saved.utils.savedInject
 import com.pluu.sample.saved.utils.savedInjectNonNull
+import timber.log.Timber
 
 class SampleActivity : ComponentActivity() {
     var requiredUser by savedInjectNonNull<UserDto>()
@@ -11,6 +13,11 @@ class SampleActivity : ComponentActivity() {
 
     var optionUser by savedInject<UserDto>()
     var optionUserLambda by savedInject<UserDto> { optionUserDto }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.d("onCreate")
+    }
 
     fun updateUser(value: UserDto) {
         requiredUser = value
--- app/src/debug/java/com/pluu/sample/saved/SampleFragment.kt
+++ app/src/debug/java/com/pluu/sample/saved/SampleFragment.kt
@@ -1,9 +1,12 @@
 package com.pluu.sample.saved
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.pluu.sample.saved.model.UserDto
 import com.pluu.sample.saved.utils.savedInject
 import com.pluu.sample.saved.utils.savedInjectNonNull
+import timber.log.Timber
 
 class SampleFragment : Fragment() {
     var requiredUser by savedInjectNonNull<UserDto>()
@@ -11,6 +14,11 @@ class SampleFragment : Fragment() {
 
     var optionUser by savedInject<UserDto>()
     var optionUserLambda by savedInject<UserDto> { optionUserDto }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.d("onViewCreated")
+    }
 
     fun updateUser(value: UserDto) {
         requiredUser = value
